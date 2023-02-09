240003196f12fca5233b7977c3c381fc986256c9
cf21-0012 <104171322+cf21-0012@users.noreply.github.com>
2023-02-08 23:50:12 -0400

    Add files via upload

---
+++ Ejercicio 1 - Fibonacci/Ejercicio 1 - Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1___Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al programa que imprime la sucesion Fibonacci");
+            Console.WriteLine("Introduce la cantidad de numeros que imprimira la sucesion ");
+            int suces = int.Parse(Console.ReadLine());
+
+            int a = 0, b = 1, c = 0;
+
+
+            for (int i = 0; i < suces; i++)
+            {
+
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+                c = a + b;
+            }
+            Console.Write("El ultimo numero de la sucesión fue " + b);
+            Console.Read();
+        }
+    }
+}
