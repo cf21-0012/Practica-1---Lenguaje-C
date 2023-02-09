d3e7f74b8d35485ece98d9cbd27f3d1da3a15ee7
cf21-0012 <104171322+cf21-0012@users.noreply.github.com>
2023-02-09 02:33:35 -0400

    Add files via upload

---
+++ Ejercicio 4 - Funciones/Ejercicio 4 - Funciones Trigonometricas/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace Ejercicio_4___Funciones_Trigonometricas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Bienvenido al programa de calculo de funciones trigonometricas");
+            menu();
+        }
+        static void menu()
+        {
+
+            Console.WriteLine("1. Seno");
+            Console.WriteLine("2. Coseno");
+            Console.WriteLine("3. Tangente");
+            Console.WriteLine("4. Cosecante");
+            Console.WriteLine("5. Secante");
+            Console.WriteLine("6. Cotangente");
+            Console.WriteLine("7. Salir");
+            Console.WriteLine("Seleccione la funcion que va a calcular:");
+            int opc = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el angulo ");
+            double a = double.Parse(Console.ReadLine());
+            
+            opciones(opc, a);
+        }
+
+       static void opciones(int opcion, double a)
+        { double result = 0;
+            switch (opcion)
+            {
+                case 1:
+                    result = Math.Sin(a);
+
+                    Console.WriteLine("El seno es " + result);
+                    Console.WriteLine("-----------------------------------------");
+
+
+                    menu();
+                    break;
+
+
+                case 2:
+                    result = Math.Cos(a);
+
+                    Console.WriteLine("El coseno es " + result);
+                    Console.WriteLine("-----------------------------------------");
+
+
+                    menu();
+                    break;
+                case 3:
+
+                    result = Math.Tan(a);
+
+                    Console.WriteLine("La tangente es " + result);
+                    Console.WriteLine("-----------------------------------------");
+
+                    menu();
+                    break;
+
+                case 4:
+                    result = Math.Sin(a);
+
+                    Console.WriteLine("La cosecante " + result);
+                    Console.WriteLine("-----------------------------------------");
+
+                    menu();
+                    break;
+
+                case 5:
+                    result = Math.Acos(a);
+
+                    Console.WriteLine("La secante es " + result);
+                    Console.WriteLine("-----------------------------------------");
+
+                    menu();
+                    break;
+
+                case 6:
+                    result = Math.Atan(a);
+
+                    Console.WriteLine("La cotangente es " + result);
+                    Console.WriteLine("-----------------------------------------");
+
+                    menu();
+                    break;
+
+                case 7:
+                    break;
+
+                default:
+                    Console.WriteLine("Por favor seleccione una opcion existente del menu");
+                    Console.WriteLine("-----------------------------------------");
+                    menu();
+
+                    break;
+
+            }
+
+        }
+    }
+}
