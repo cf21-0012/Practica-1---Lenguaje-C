8c798358511940d37d7752f3df334846419346ea
cf21-0012 <104171322+cf21-0012@users.noreply.github.com>
2023-02-13 10:03:29 -0400

    Add files via upload

---
+++ Ejercicio 0 - Factorial/Ejercicio 0 - Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_0___Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa que imprime el factorial de un numero");
+            Console.WriteLine("Introduzca el numero:");
+            int fac = 1; int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                fac *= i;
+            }
+
+            Console.WriteLine("El factorial es " +fac);
+            Console.ReadLine();
+        }
+    }
+}
